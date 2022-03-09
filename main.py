13403d453c68ef3b52ebecfe9cd8bc295e8cd525
Gustavo Ghioldi <gustavoghioldi@gmail.com>
2022-03-09 19:35:59 -0300

    acomodamiento

--- main.py
+++ main.py
@@ -25,26 +25,33 @@ window = tk.Tk()
 ### client #######
 label_client_name = tk.Label(text="Name:")
 label_client_dni = tk.Label(text="DNI:")
+
+label_client_name.place(x=10, y=50)
+label_client_dni.place(x=310, y=50)
+
+
 entry_client_name = tk.Entry()
 entry_client_dni = tk.Entry()
+entry_client_name.place(x=110, y=50)
+entry_client_dni.place(x=410, y=50)
+
 btn_create_client = tk.Button(text="Crear Cliente", command=add_client, width=20)
-label_client_name.place(x=10, y=50)
-label_client_dni.place(x=10, y=150)
-entry_client_name.place(x=200, y=50)
-entry_client_dni.place(x=200, y=150)
-btn_create_client.place(x=240, y=300)
+btn_create_client.place(x=240, y=100)
 ### end client ###
+
 ### product ######
 label_product_name = tk.Label(text="Name:")
-label_product_price = tk.Label(text="DNI:")
+label_product_price = tk.Label(text="price:")
+label_product_name.place(x=10, y=200)
+label_product_price.place(x=310, y=200)
+
 entry_product_name = tk.Entry()
 entry_product_price = tk.Entry()
+entry_product_name.place(x=110, y=200)
+entry_product_price.place(x=410, y=200)
+
 btn_create_product = tk.Button(text="Crear Product", command=add_product, width=20)
-label_product_name.place(x=10, y=450)
-label_product_price.place(x=10, y=550)
-entry_product_name.place(x=200, y=550)
-entry_product_price.place(x=200, y=550)
-btn_create_product.place(x=240, y=600)
+btn_create_product.place(x=240, y=250)
 ### end product ##
 
 ### sales ########
@@ -54,13 +61,20 @@ combo_clients = Combobox(
 combo_products = Combobox(
     values = ['chau']
 )
-
 entity_sale_qty = tk.Entry()
 
-combo_clients.place(x=10, y=100)
+combo_clients.place(x=10, y=400)
+combo_products.place(x=210, y=400)
+entity_sale_qty.place(x=410, y=400)
+label_client = tk.Label(text="client:")
+label_product = tk.Label(text="product:")
+label_qty = tk.Label(text="Qty:")
+label_client.place(x=10, y=350)
+label_product.place(x=210, y=350)
+label_qty.place(x=410, y=350)
 btn_create_sale = tk.Button(text="Crear Venta", command=add_sale, width=20)
-btn_create_sale.place(x=240, y=900)
+btn_create_sale.place(x=240, y=450)
 ### end sale #####
 
-window.config(width=700, height=1000)
+window.config(width=700, height=500)
 window.mainloop()
